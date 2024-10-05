cd5e5d16236c8251c934f22ac1c71febd5f0b965
SERGIY KOLESNYK <114981277+marakoris2022@users.noreply.github.com>
2024-10-06 01:59:31 +0300

    feat: fs-create done

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,20 @@
+import { appendFile, readFile } from "node:fs/promises";
+import { join } from "node:path";
+
 const create = async () => {
-    // Write your code here 
+  const basePath = import.meta.dirname;
+  const filePath = join(basePath, "./files/fresh.txt");
+
+  try {
+    const fileData = await readFile(filePath);
+    if (fileData) throw new Error("FS operation failed");
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      await appendFile(filePath, "I am fresh and young");
+    } else {
+      console.error(error);
+    }
+  }
 };
 
 await create();
