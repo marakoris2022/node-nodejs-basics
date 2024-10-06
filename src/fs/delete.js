86444b4989d11abeffbcca03cfa1ab5adc823165
SERGIY KOLESNYK <114981277+marakoris2022@users.noreply.github.com>
2024-10-06 11:32:21 +0300

    feat: delete completed

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,20 @@
+import { readdir, unlink } from "node:fs/promises";
+import { join } from "node:path";
+
 const remove = async () => {
-    // Write your code here 
+  const basePath = import.meta.dirname;
+
+  try {
+    const fileData = await readdir(join(basePath, "files"));
+
+    if (!fileData.includes("fileToRemove.txt")) {
+      throw new Error("FS operation failed");
+    }
+
+    await unlink(join(basePath, "files", "fileToRemove.txt"));
+  } catch (error) {
+    console.error(error.message);
+  }
 };
 
 await remove();
